cdea26f10ddbd7f76a5ac34469d77d0592dfc0a7
Vashti <patrena@sbcglobal.net>
2021-10-07 18:42:52 -0700

    client and intergration completed

--- server/server.js
+++ server/server.js
@@ -14,10 +14,24 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
+const appmiddle = async() => {
+  await server.start()
+  server.applyMiddleware({ app });
+}
+appmiddle()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
