f970b033a44b126661e419113febd264f383fd3c
Vashti <patrena@sbcglobal.net>
2021-10-08 21:32:48 -0700

    completed 5.7

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
 
-const { typeDefs, resolvers } = require('./server/schemas');
-const { authMiddleware } = require('./server/utils/auth');
-const db = require('./server/config/connection');
+// const { typeDefs, resolvers } = require('./server/schemas');
+// const { authMiddleware } = require('./server/utils/auth');
+// const db = require('./server/config/connection');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware
+// });
+// // const appmiddle = async() => {
+// //   await server.start()
+// //   server.applyMiddleware({ app });
+// // }
+// // appmiddle()
+
 // const appmiddle = async() => {
 //   await server.start()
 //   server.applyMiddleware({ app });
 // }
 // appmiddle()
 
-const appmiddle = async() => {
-  await server.start()
-  server.applyMiddleware({ app });
-}
-appmiddle()
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
--- server/server.js
+++ server/server.js
@@ -1,25 +1,20 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware 
 });
 
-// server.applyMiddleware({ app });
-const appmiddle = async() => {
-  await server.start()
-  server.applyMiddleware({ app });
-}
-appmiddle()
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
